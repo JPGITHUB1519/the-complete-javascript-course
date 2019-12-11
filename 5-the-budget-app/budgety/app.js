63f593b73a1e72e22348db3de613ec30e7b43ff9
Jean Urena <juanpedro1519@gmail.com>
2019-12-10 23:22:35 -0400

    S5- Setting up the First Event Listeners

--- 5-the-budget-app/budgety/app.js
+++ 5-the-budget-app/budgety/app.js
@@ -4,37 +4,42 @@
   IIFE(Anonymous function + autoexecuted) that returns an object
 */
 
+// Budget Controller
 var budgetController = (function() {
 
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  }
-
-  // returning object to expose the public methods
-  // this object will be asigned to the budgetController Variable
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
 })();
 
 
+
+// UI Controller
 var UIController = (function() {
   // some code
 })();
 
 
+
+// Global APP Controller
 // the model and the UI are separate modules and should not communicate each other
 // this controller is for join the model and the UI
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetController.publicTest(5);
-
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
+  
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the Budget
+    // 5. Display the budget on the UI
+    console.log('It works!');
   }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+
+    // which is for older browser
+    if (event.keyCode === 13 || event.which === 13) {
+      console.log('Enter was pressed');
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
