7d0155e71ffbac0dcb0d2b27328d3ac4fd87955c
Jean <juanpedro1519@gmail.com>
2019-12-19 10:33:17 -0400

    S6- Updating the Percentages: Budget Controller

--- 5-the-budget-app/budgety/app.js
+++ 5-the-budget-app/budgety/app.js
@@ -11,7 +11,20 @@ var budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
   };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+    if (totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+    } else {
+      this.percentage = -1;
+    }
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
+  }
 
   var Income = function(id, description, value) {
     this.id = id;
@@ -98,6 +111,30 @@ var budgetController = (function() {
       } else {
         data.percentage = -1;
       }
+    },
+
+    calcPercentages: function() {
+      /*
+        a = 20,
+        b = 10
+        c = 40
+        income = 100
+        a = 20 / 100 = 20%
+        b = 10 / 100 = 10%
+        c = 40 / 100 = 40%
+      */
+
+      data.allItems.exp.forEach(function(cur) {
+        cur.calcPercentage(data.totals.inc);
+      });
+    },
+
+    getPercentages: function() {
+      var allPerc = data.allItems.exp.map(function(cur) {
+        return cur.getPercentage();
+      });
+      
+      return allPerc
     },
 
     getBudget: function() {
@@ -247,10 +284,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var updatePercentages = function() {
     // 1. Calculate Percentages
-
+    budgetCtrl.calcPercentages();
+    
     // 2. Read Percentages from the budget controller
+    var percentages = budgetCtrl.getPercentages();
 
     // 3. Update the UI with the new Percentages
+    console.log(percentages);
   }
 
   var ctrlAddItem = function() {
