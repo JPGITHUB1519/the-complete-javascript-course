022980d65e6219f48fa2b4fb9e18061a6b2f6561
Jean <juanpedro1519@gmail.com>
2020-03-02 21:30:39 -0400

    How ES6 Modules Work

--- 8-forkify/src/js/index.js
+++ 8-forkify/src/js/index.js
@@ -1,6 +1,15 @@
 // App Controller
-import num from './test';
 
-const x = 23;
+// default import
+import str from './models/Search';
 
-console.log(`I imported ${num} from another module test! Variable x is ${x}`);
+// name import
+import { add as sum, multiply, ID } from './views/searchView';
+
+import * as searchView from './views/searchView';
+
+console.log(str);
+console.log(sum(2, 3));
+console.log(ID);
+
+console.log(`Using exported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}, ${str}`)
