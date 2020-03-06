0f3cc72102d9faf44f2774979dde9398ddfbea91
Jean <juanpedro1519@gmail.com>
2020-03-06 18:34:24 -0400

    Forkify - Rendering an AJAX Loading Spinner

--- 8-forkify/src/js/index.js
+++ 8-forkify/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 
 /** Global State of the app
@@ -24,12 +24,14 @@ const controlSearch = async () => {
     // 3) Prepare the UI
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) render results on the UI
     console.log(state.search.result);
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 };
